1841339ef5314d71b66b2aed34fb4206ad15714b
trungtashi99 <trungtashi@gmai.com>
2022-08-12 15:30:40 +0700

    Bài tập Stop watch

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+    StopWatch test = new StopWatch();
+    test.start();
+    test.stop();
+    System.out.println(test.getElapsedTime());
+}
+
+}
